1143bad490da8b3636faba6de11c7b5999e36267
Mattuser <75076355+Mattuser@users.noreply.github.com>
2023-11-13 17:05:55 +0000

    alteração na página inicial

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Clone tab news - by Matt</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste");
-}
-
 export default Home;
