19c12ccb8a91a4f6b46ff6d3603212561aca8840
Mattuser <75076355+Mattuser@users.noreply.github.com>
2023-11-13 13:20:37 +0000

    adiciona scripts `lint:check` e `lint>fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Clone tab news - by Matt</h1>
+  return <h1>Clone tab news - by Matt</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
 }
 
 export default Home;
