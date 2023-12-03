47fae69e04a930dfb9ffa67a9b79217dc5f0f198
Mattuser <75076355+Mattuser@users.noreply.github.com>
2023-12-03 19:11:59 +0000

    add information from db and test these properties

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const versionQuery = await database.query(
+    "SELECT split_part(version(), ' ', 2) AS postgres_version;",
+  );
+  const maxConnectionsQuery = await database.query("SHOW MAX_CONNECTIONS;");
+
+  const activeConnectionsQuery = await database.query(
+    "SELECT count(distinct(numbackends)) FROM pg_stat_database;",
+  );
+
+  const version = parseFloat(versionQuery.rows[0].postgres_version);
+  const maxConnections = new Number(
+    maxConnectionsQuery.rows[0].max_connections,
+  );
+  const activeConnections = new Number(activeConnectionsQuery.rows[0].count);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: version,
+    max_connections: maxConnections,
+    active_connections: activeConnections,
+  });
 }
 
 export default status;
--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.active_connections).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.max_connections).toBeGreaterThan(
+    responseBody.active_connections,
+  );
 });
